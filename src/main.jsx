47fefa864c84810919044deadf540c65f28a8431
Samantha Reape <54642592+SweetReap@users.noreply.github.com>
2024-04-12 14:42:51 -0400

    finished react-dom-tutorial

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root, {loader as rootLoader, action as rootAction} from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact, {loader as contactLoader} from "./routes/contact";
+import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
+import EditContact, {action as editAction} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 
 const router = createBrowserRouter([
   {
@@ -15,9 +18,27 @@ const router = createBrowserRouter([
     action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader
+        errorElement: <ErrorPage/>,
+        children: [
+          {index: true, element: <Index />},
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact/>,
+            loader: contactLoader, 
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div> Oops! There was an error. </div>,
+          },
+        ],
       },
     ],
   },
